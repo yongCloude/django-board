e54a9577c5f14c5c110a22570ef8d6b0ce5a54a9
yongCloude <djkye0001@naver.com>
2022-05-24 13:51:37 +0900

    [feat] 회원등록 View

--- auths/views.py
+++ auths/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .serializers import ResisterSerializer
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView) :
+    queryset = User.objects.all()
+    serializer_class = ResisterSerializer
+    
+
