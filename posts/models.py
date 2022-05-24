e3efefb0a31d9500a4cf2a07784d404e773ca894
yongCloude <djkye0001@naver.com>
2022-05-24 19:59:34 +0900

    [create] posts Model 추가

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from users.models import Profile
 
 # Create your models here.
+
+class Post(models.Model) :
+    author = models.ForeignKey(User, on_delete=models.CASCADE,)
+    profile = models.ForeignKey(Profile, on_delete = models.CASCADE, blank = True)
+    title = models.CharField(max_length = 128)
+    category = models.CharField(max_lenght = 128)
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'post/', default = 'default.png')
+    likes = models.ManyToManyField(User)
+    published_date = models.DateTimeField(default = timezone.now)
+    
+    
+
