c896cdc12cc0ca5a7a76981c08a7ad074a1761b3
yongCloude <djkye0001@naver.com>
2022-05-24 13:45:44 +0900

    [feat] email, password 검증 및 Model 정의

---
+++ auths/serializers.py
@@ -0,0 +1,34 @@
+from tkinter.ttk import _TreeviewColumnDict
+from turtle import Turtle
+from django.contrib.auth.models import  User
+from django.contrib.auth.password_validation import validate_password
+
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
+
+
+class ResisterSerializer(serializers.ModelSerializer) :
+    email = serializers.EmailField(
+        required = True,
+        validators = [UniqueValidator(queryset = User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only = True,
+        required = True,
+        validators = [validate_password],
+    )
+    password2 = serializers.CharField(write_only = True, required = True)
+
+    class Meta :
+        modle = User
+        fields = ('username', 'password', 'password2', 'email')
+
+        def validate(self, data) :
+            if data['password '] != data['password2'] :
+                raise serializers.ValidationError(
+                    {"password" : "Password fields didn't match."}
+                )
+
+            return data
+            
