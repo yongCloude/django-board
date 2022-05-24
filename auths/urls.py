d09da821b8e773102d34a198c00c5e1b75448957
yongCloude <djkye0001@naver.com>
2022-05-24 15:51:53 +0900

    [feat] Profile Urls

--- auths/urls.py
+++ auths/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginView, RegisterView
+from .views import LoginView, RegisterView, ProfileView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()), 
+    path('profile/<int:pk>/', ProfileView.as_view()),
 ]
