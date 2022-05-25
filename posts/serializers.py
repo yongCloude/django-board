9db838ef431c931f2fef6e9ad05e7d806f73550f
yongCloude <djkye0001@naver.com>
2022-05-25 15:02:18 +0900

    [feat] Comment 시리얼라이저 추가

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,14 +1,29 @@
+from dataclasses import field
 from rest_framework import serializers
 
 from auths.serializers import ProfileSerializer
-from .models import Post
+from .models import Post, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer) :
+    profile = ProfileSerializer(read_only = True)
+
+    class Meta :
+        model = Comment
+        fields = ("pk", "profile", "post", "text")
+    
+class CommentCreateSerializer(serializers.ModelSerializer) :
+    class Meta :
+        model = Comment
+        fields = ("post", "text")
+
 
 class PostSerializer(serializers.ModelSerializer) :
     profile = ProfileSerializer(read_only = True)
-    
+    comments = CommentSerializer(many = True, read_only = True)
     class Meta :
         model = Post
-        fields = ("pk", "profile", "title", "body", "image", "published_date", "likes")
+        fields = ("pk", "profile", "title", "body", "image", "published_date", "likes", "commnets")
         
 class PostCreateSerializer(serializers.ModelSerializer) :
     class Meta :
